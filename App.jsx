e4b69871516e116ca95bfc182ec02dea107c4651
Deiven_Tibo <de.ivergarciacarrillo42@gmail.com>
2025-07-24 21:54:44 -0500

    los 4 fantasticos solo en cines

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Register from '../src/components/register';
+
+
+function App() {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+}
+
+export default App;
+// Deiven el millonario
